b4544d4fb1bcb8b9ef838c5a361a48c3ec0b14ae
Ben Rossi <ben@rossinet.com>
2021-05-04 14:07:59 -0400

    Clean up styles for contact form and turn off fade-in effect on primary logo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -16,9 +16,9 @@
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
+			//window.setTimeout(function() {
+			//	$body.removeClass('is-preload');
+			//}, 1);
 		});
 
 	// Nav.
